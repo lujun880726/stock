5932fe46a2436cab32924ce23f4e182001bdfd11
LJ <jun.lu.726@gmail.com>
2014-05-13 17:58:26 +0800

    D

--- app/corn/loadNewStock.php
+++ app/corn/loadNewStock.php
@@ -7,7 +7,6 @@ $objBase = m('m_base');
 
 //添加每天新的股票
 $page = 100;
-$page = 0;
 $url  = 'http://stock.gtimg.cn/data/index.php?appn=rank&t=ranka/chr&o=0&l=80&v=list_data&p=';
 for ($i = 1; $i <= $page; $i++) {
     $endUrl = $url . $i;
@@ -34,16 +33,38 @@ for ($i = 1; $i <= $page; $i++) {
 // 每天的收盘信息 停盘不记录
 $list = $objBase->db->get_all("select * from `stock` where 1");
 foreach ($list as $val) {
-    $url = "http://qt.gtimg.cn/r=" . (0.28181632646317246 + '0.' . rand(1, 999999999)) . "q=marketStat,stdunixtime,sh600160,";
-
+    $url = "http://qt.gtimg.cn/r=" . (0.28181632646317246 + '0.' . rand(1, 999999999)) . "q=marketStat,stdunixtime,sh600432,";
+    $data = '';
     $data = file_get_contents($url);
-    $data = 'v_marketStat="2014-05-13 17:25:01|HK_close_已收盘|SH_close_已收盘|SZ_close_已收盘|US_close_未开盘|SQ_close_已休市|DS_close_已休市|ZS_close_已休市"; v_stdunixtime="1399973155"; v_sh600160="1~巨化股份~600160~5.80~5.27~5.80~1382253~571110~811144~5.80~5885~5.79~826~5.78~362~5.77~265~5.76~571~0.00~0~0.00~0~0.00~0~0.00~0~0.00~0~14:59:58/5.80/4/S/2320/14575|14:59:58/5.80/39/S/22620/14572|14:59:38/5.80/30/S/17400/14555|14:59:33/5.80/91/S/52780/14549|14:59:28/5.80/240/S/139200/14546|14:59:28/5.80/4/S/1822/14542~20140513150353~0.53~10.06~5.80~5.66~5.80/1381562/796958265~1382253~79736~7.72~1334.36~~5.80~5.66~2.66~103.91~105.03~1.41~5.80~4.74~"; ';
+    if(empty($data)){
+        sleep(5);
+        $data = file_get_contents($url);
+    }
+    if (empty($data)){
+        file_put_contents(date('Y-m-d') . '.log', $val['stock_id']."\r\n", FILE_APPEND);
+        continue;
+    }
 
-    print_r($data);
-    exit;
+    $tmp  = explode('~', $data);
+    if (!$val['name']) {
+        $objBase->db->update('stock', array('name' => $tmp[1]), "stock_id = '" . $val['stock_id'] . "'");
+    }
+    $dayArr = array(
+        'stock_id'  => $val['stock_id'],
+        'type'      => $val['type'],
+        'day_time'  => date('Ymd'),
+        'week_time' => date('Ymd'),
+        'mon_time'  => date('Ym'),
+        'year_time' => date('Y'),
+        'open'      => $tmp[5],
+        'top'       => $tmp[33],
+        'footer'    => $tmp[34],
+        'harvest'   => $tmp[3],
+        'vol'       => $tmp[6],
+    );
+
+    $objBase->db->insert('day_harvest_info', $dayArr);
 }
-var_dump($list);
-exit;
 
 // 星期五做周信息
 
