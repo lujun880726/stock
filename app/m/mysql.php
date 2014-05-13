8f78e42c32d2cc9e9b32aad2d2575378030bb0c1
LJ <jun.lu.726@gmail.com>
2014-05-13 17:59:00 +0800

    D

--- app/m/mysql.php
+++ app/m/mysql.php
@@ -14,7 +14,7 @@ Class m_mysql
         $this->time               = $this->microtime_float();
         $db_config["hostname"]    = "localhost"; //服务器地址
         $db_config["username"]    = "root"; //数据库用户名
-        $db_config["password"]    = "sql123"; //数据库密码
+        $db_config["password"]    = "PassWord"; //数据库密码
         $db_config["database"]    = "stock"; //数据库名称
         $db_config["charset"]     = "utf8"; //数据库编码
         $db_config["pconnect"]    = 1; //开启持久连接
